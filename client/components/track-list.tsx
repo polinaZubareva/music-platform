dc62c728d0473a7b77730d6cd0fd015c01e233d4
kirillpuzanov <k.puzanov@3divi.com>
2022-06-23 20:49:03 +0500

    Added track list

---
+++ client/components/track-list.tsx
@@ -0,0 +1,21 @@
+import { Box, Grid } from '@mui/material';
+import { ITrack } from '../types/track';
+import TrackItem from './track-item';
+
+interface TrackListProps {
+  tracks: ITrack[];
+}
+
+const TrackList: React.FC<TrackListProps> = ({ tracks }) => {
+  return (
+    <Grid container direction='column'>
+      <Box p={2}>
+        {tracks.map((track) => (
+          <TrackItem key={track._id} track={track} />
+        ))}
+      </Box>
+    </Grid>
+  );
+};
+
+export default TrackList;
