907364a2bdef3f35f50bb5c6a518a64001d0c40f
Polina <zubarevapolinav@gmail.com>
2022-06-23 19:34:31 +0500

    init

---
+++ server/src/main.ts
@@ -0,0 +1,19 @@
+import {NestFactory} from "@nestjs/core";
+import {AppModule} from "./app.module";
+import mongoose from "mongoose";
+
+
+const start = async () => {
+    try {
+        const PORT = process.env.PORT || 5000;
+        const app = await NestFactory.create(AppModule);
+        app.enableCors();
+        mongoose.connect("localhost:27017")
+            .then(await app.listen(PORT, () => console.log(`server started on PORT ${PORT}`))            )
+
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+start()
