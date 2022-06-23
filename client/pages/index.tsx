7be4537950eb59493a240368479d78be1b59db13
kirillpuzanov <k.puzanov@3divi.com>
2022-06-23 20:07:52 +0500

    init

---
+++ client/pages/index.tsx
@@ -0,0 +1,24 @@
+function Index() {
+  return (
+    <>
+      <div className="center">
+        <h1>Добро пожаловать</h1>
+        <h3>Здесь собраны лучшие треки</h3>
+      </div>
+
+      <style jsx>
+        {`
+          .center {
+            margin-top: 150px;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+          }
+        `}
+      </style>
+    </>
+  );
+}
+
+export default Index;
